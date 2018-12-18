0778f7a081c2383d9f4c8bd5f7c3d3e933dda215
Muhammad Ardhana Wahyu Nugraha Muhammad Ardhana Wahyu Nugraha <mardhanawn@gmail.com>
2018-12-18 16:51:04 +0700

    CSS Home, Profile.js, Header.js, Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="Hom-footer">
+                    <div> Copyright Reserved 2018 </div>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Footer;
--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Home.css';
-import profile from './src/profile.jpg'
+import Header from './Header.js';
+import Profile from './Profile.js';
+import Footer from './Footer.js';
+import profile from './profile.jpeg';
 
 class Home extends Component {
   render() {
     return (
       <div className="Hom">
-        <header className="Hom-header">
-            <nav>
-              <ul>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">PROFILE</a></li>
-                <li><a href="#">ABOUT</a></li>
-              </ul>
-            </nav>
-        </header>
+        <Header />
         <body className="Hom-body">
           <section id="box-profile">
           <div className="img-profile">
@@ -24,12 +19,16 @@ class Home extends Component {
               </div>
           </div>
           <div class="description">
-              <h1>Muhammad Ardhana Wahyu Nugraha</h1>
-              <p>Front End Developer</p>
-              <a href="#" className="button bg-green">Edit</a>
-              <a href="#" className="button border-blue">Resume</a>
+            <h1>Muhammad Ardhana Wahyu Nugraha</h1>
+            <p>Front End Developer</p>
+            <a href="#" class="button border-blue">LinkedIn</a>
+			<a href="#" class="button bg-green"> Profile</a>
           </div>
           <div className="information">
+          <div className="data">
+                  <p className="field">Nickname</p>
+                  <p className="text-gray">Ardhana</p>
+        </div>
               <div className="data">
                   <p className="field">Place, Date of Birth</p>
                   <p className="text-gray">Purwokerto, Banyumas, 17 Mei 2002</p>
@@ -56,9 +55,9 @@ class Home extends Component {
           <div className="Hom-batch"> Batch </div>
           <button type="button" class="btn btn-primary">Start Test</button>
         </body>
-        <footer className="Hom-footer">
-          <div> Copyright Reserved 2018 </div>
-        </footer>
+       <div>
+            <Footer />
+        </div>
       </div>
     );
   }
